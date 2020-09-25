a41287c7ab9099d1c0286538496a428ae5588bf7
NoLifeKing <webmaster@itssimple.se>
2020-09-25 19:56:39 +0200

    feat: Added Hangfire for background jobs

---
+++ Journal-Limpet.Hangfire/Program.cs
@@ -0,0 +1,25 @@
+﻿using Journal_Limpet.Shared;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Journal_Limpet.Hangfire
+{
+    class Program
+    {
+        static IServiceCollection _serviceCollection;
+        static IConfiguration _configuration;
+
+        static void Main(string[] args)
+        {
+            _configuration = new ConfigurationBuilder()
+               .AddJsonFile("Journal-Limpet.environment", optional: true, reloadOnChange: true)
+               .AddEnvironmentVariables()
+               .AddCommandLine(args)
+               .Build();
+
+            _serviceCollection = new ServiceCollection();
+
+            _serviceCollection.AddJournalLimpetDependencies(_configuration);
+        }
+    }
+}
