f5629c98ac7c56ec8740571dd170c2691704ea9e
NoLifeKing <webmaster@itssimple.se>
2020-12-25 04:23:14 +0100

    feat: Added ImportantJobs and RedisJobLock

---
+++ Journal-Limpet/RedisJobLock.cs
@@ -0,0 +1,102 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Journal_Limpet
+{
+    public class RedisJobLock : IDisposable
+    {
+        private string TypeName { get; set; }
+        private string Handle { get; set; }
+
+        private IDatabase RDB { get { return getRedisDatabase(); } }
+        private Guid LockInstance;
+        private CancellationToken _cancellationToken;
+        private readonly CancellationTokenSource CTS;
+
+        public RedisJobLock(string lockName)
+        {
+            CTS = new CancellationTokenSource();
+
+            TypeName = lockName;
+            LockInstance = Guid.NewGuid();
+            _cancellationToken = CTS.Token;
+
+            Handle = GetHandle(TypeName);
+        }
+
+        public static bool IsLocked(string lockName)
+        {
+            var rdb = getRedisDatabase();
+            var handle = GetHandle(lockName);
+            return rdb.LockQuery(handle) != RedisValue.Null;
+        }
+
+        private static string GetHandle(string lockName)
+        {
+            return $"joblock:{lockName}";
+        }
+
+        private static IDatabase getRedisDatabase()
+        {
+            return SharedSettings.RedisClient.GetDatabase(0);
+        }
+
+        TimeSpan LockTime = TimeSpan.FromSeconds(15);
+
+        Task _refreshLockTask;
+        async Task RefreshLockAsync()
+        {
+            try
+            {
+                await Task.Delay(LockTime.Subtract(TimeSpan.FromMilliseconds(LockTime.TotalMilliseconds / 2)), _cancellationToken);
+                KeepLocked();
+                _refreshLockTask = RefreshLockAsync();
+                _ = PubSubLog($"Refreshing lock {Handle} / {LockInstance}");
+            }
+            catch (TaskCanceledException)
+            {
+                /* This is totally ok, we cancelled it */
+            }
+        }
+
+        private void KeepLocked()
+        {
+            RDB.LockExtend(Handle, LockInstance.ToString(), LockTime);
+        }
+
+        public async Task KeepLockedAsync()
+        {
+            await RDB.LockExtendAsync(Handle, LockInstance.ToString(), LockTime);
+        }
+
+        public bool TryTakeLock()
+        {
+            var couldTakeLock = RDB.LockTake(Handle, LockInstance.ToString(), LockTime);
+            if (couldTakeLock) _refreshLockTask = RefreshLockAsync();
+            _ = PubSubLog($"Could {(couldTakeLock ? "" : "not ")}take lock for {Handle} / {LockInstance}");
+            return couldTakeLock;
+        }
+
+        public async Task<bool> TryTakeLockAsync()
+        {
+            var couldTakeLock = await RDB.LockTakeAsync(Handle, LockInstance.ToString(), LockTime);
+            if (couldTakeLock) _refreshLockTask = RefreshLockAsync();
+            _ = PubSubLog($"Could {(couldTakeLock ? "" : "not ")}take lock for {Handle} / {LockInstance}");
+            return couldTakeLock;
+        }
+
+        internal async Task PubSubLog(string logmessage)
+        {
+            await SharedSettings.RedisClient.GetSubscriber().PublishAsync("LockMessages/JournalLimpet", logmessage);
+        }
+
+        public void Dispose()
+        {
+            _ = PubSubLog($"Releasing lock {Handle} / {LockInstance}");
+            RDB.LockRelease(Handle, LockInstance.ToString());
+            CTS.Cancel();
+        }
+    }
+}
---
+++ Journal-Limpet/SharedSettings.cs
@@ -0,0 +1,76 @@
+﻿using Hangfire;
+using Hangfire.Logging;
+using StackExchange.Redis;
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Journal_Limpet
+{
+    public static class SharedSettings
+    {
+        public static TimeZoneInfo SwedishTimeZone => TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+
+        private static int _jobCount = 100;
+
+        private static ConnectionMultiplexer _redisClient;
+
+        public static ConnectionMultiplexer RedisClient => _redisClient ?? (_redisClient = ConnectionMultiplexer.Connect("localhost:6379"));
+
+        public static bool CheckIfTaskIsScheduledOrInProgress(string taskName, string methodName)
+        {
+            if (RedisJobLock.IsLocked($"{taskName}.{methodName}"))
+            {
+                return true;
+            }
+
+            var mapi = JobStorage.Current.GetMonitoringApi();
+
+            var processing = mapi.ProcessingJobs(0, _jobCount).Where(i => i.Value.Job != null).Select(i => string.Format("{0}.{1}", i.Value.Job.Method.DeclaringType.Name, i.Value.Job.Method.Name));
+            var scheduled = mapi.ScheduledJobs(0, _jobCount).Where(i => i.Value.Job != null).Select(i => string.Format("{0}.{1}", i.Value.Job.Method.DeclaringType.Name, i.Value.Job.Method.Name));
+            var enqueued = mapi.EnqueuedJobs("default", 0, _jobCount).Where(i => i.Value.Job != null).Select(i => string.Format("{0}.{1}", i.Value.Job.Method.DeclaringType.Name, i.Value.Job.Method.Name));
+
+            var jobs = processing.Concat(scheduled).Concat(enqueued);
+
+            return jobs.Any(s => s == $"{taskName}.{methodName}");
+        }
+
+        public static void AddImportantJob(string className, string methodName, Expression<Action> action, TimeSpan startInterval, TimeSpan? subsequentInterval = null)
+        {
+            if (ImportantJobs.TryAdd(className + ":" + methodName, (className, methodName, action, startInterval, subsequentInterval ?? startInterval)) &&
+                !CheckIfTaskIsScheduledOrInProgress(className, methodName))
+            {
+                BackgroundJob.Schedule(action, startInterval);
+            }
+        }
+
+        public static void AddImportantJob(string className, string methodName, Expression<Func<Task>> action, TimeSpan startInterval, TimeSpan? subsequentInterval = null)
+        {
+            if (ImportantJobs.TryAdd(className + ":" + methodName, (className, methodName, action, startInterval, subsequentInterval ?? startInterval)) &&
+                !CheckIfTaskIsScheduledOrInProgress(className, methodName))
+            {
+                BackgroundJob.Schedule(action, startInterval);
+            }
+        }
+
+        public static ConcurrentDictionary<string, (string className, string methodName, object action, TimeSpan startInterval, TimeSpan subsequentInterval)> ImportantJobs =
+            new ConcurrentDictionary<string, (string className, string methodName, object action, TimeSpan startInterval, TimeSpan subsequentInterval)>();
+
+        public static ILog GetExceptionalLogger()
+        {
+            return LogProvider.GetCurrentClassLogger();
+        }
+
+        public static string GetResourceFullName(string resourceName)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            string[] resourceCollection = assembly.GetManifestResourceNames();
+
+            return resourceCollection.FirstOrDefault(i => i.Contains(resourceName));
+        }
+    }
+}
