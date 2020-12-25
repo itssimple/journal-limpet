f28a92baec7b3dd856e4a3506ea357cb05c7581d
NoLifeKing <webmaster@itssimple.se>
2020-12-25 04:26:26 +0100

    feat: BaseObject for Json-things

---
+++ Journal-Limpet.Shared/Models/EliteBaseJsonObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Journal_Limpet.Shared.Models
+{
+    public class EliteBaseJsonObject
+    {
+        [JsonExtensionData]
+        public Dictionary<string, object> ExtensionData { get; set; }
+    }
+}
