4676244fc298d2f4cbc6dc49aa71bab6f1e31a9e
NoLifeKing <webmaster@itssimple.se>
2020-12-28 21:48:54 +0100

    feat: If no html-body is sent, send textbody and replace newlines

--- Journal-Limpet.Shared/MailSender.cs
+++ Journal-Limpet.Shared/MailSender.cs
@@ -7,8 +7,13 @@ namespace Journal_Limpet.Shared
 {
     public static class MailSender
     {
-        public static async Task SendSingleEmail(IConfiguration configuration, string fromAddress, string subject, string mailBody, string mailBodyHtml)
+        public static async Task SendSingleEmail(IConfiguration configuration, string fromAddress, string subject, string mailBody, string mailBodyHtml = null)
         {
+            if (string.IsNullOrWhiteSpace(mailBodyHtml))
+            {
+                mailBodyHtml = mailBody.Replace("\n", "<br />\n");
+            }
+
             var sendgridClient = new SendGridClient(configuration["SendGrid:ApiKey"]);
             var mail = MailHelper.CreateSingleEmail(
                 new EmailAddress(fromAddress),
