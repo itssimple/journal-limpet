9e8f1cf7647117e83e118ff3b964f8213a028ac6
NoLifeKing <webmaster@itssimple.se>
2021-01-03 10:51:29 +0100

    fix: Added retry-policy for sending activity updates

--- Journal-Limpet/SSEActivitySender.cs
+++ Journal-Limpet/SSEActivitySender.cs
@@ -1,4 +1,7 @@
 ﻿using Journal_Limpet.Shared.Database;
+using Polly;
+using Polly.Retry;
+using StackExchange.Redis;
 using System;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -16,7 +19,7 @@ namespace Journal_Limpet
                 @class
             };
 
-            await SharedSettings.RedisPubsubSubscriber.PublishAsync("global-activity", JsonSerializer.Serialize(msg));
+            await GetRedisRetryPolicy().ExecuteAsync(() => SharedSettings.RedisPubsubSubscriber.PublishAsync("global-activity", JsonSerializer.Serialize(msg)));
         }
 
         public static async Task SendUserActivityAsync(Guid userIdentifier, string title, string message, string @class = "warning")
@@ -28,13 +31,20 @@ namespace Journal_Limpet
                 @class
             };
 
-            await SharedSettings.RedisPubsubSubscriber.PublishAsync($"user-activity-{userIdentifier}", JsonSerializer.Serialize(msg));
+            await GetRedisRetryPolicy().ExecuteAsync(() => SharedSettings.RedisPubsubSubscriber.PublishAsync($"user-activity-{userIdentifier}", JsonSerializer.Serialize(msg)));
         }
 
         public static async Task SendStatsActivityAsync(MSSQLDB _db)
         {
             var stats = await SharedSettings.GetIndexStatsAsync(_db);
-            await SharedSettings.RedisPubsubSubscriber.PublishAsync($"stats-activity", JsonSerializer.Serialize(stats));
+            await GetRedisRetryPolicy().ExecuteAsync(() => SharedSettings.RedisPubsubSubscriber.PublishAsync($"stats-activity", JsonSerializer.Serialize(stats)));
+        }
+
+        private static AsyncRetryPolicy<long> GetRedisRetryPolicy()
+        {
+            return Policy<long>
+                .Handle<RedisTimeoutException>()
+                .WaitAndRetryAsync(10, attempt => TimeSpan.FromSeconds(1));
         }
     }
 }
