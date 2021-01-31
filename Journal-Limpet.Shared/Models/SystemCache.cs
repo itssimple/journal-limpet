408414481e1d1530eb0697746554e9d6c3b77a57
NoLifeKing <webmaster@itssimple.se>
2021-01-31 09:42:41 +0100

    refactor: Moving system cache to shared cache-class

--- Journal-Limpet.Shared/Models/SystemCache.cs
+++ Journal-Limpet.Shared/Models/SystemCache.cs
@@ -1,4 +1,12 @@
-﻿namespace Journal_Limpet.Shared.Models
+﻿using Journal_Limpet.Shared.Models.Journal;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Journal_Limpet.Shared.Models
 {
     public enum RequiredPropertiesForCache
     {
@@ -7,5 +15,108 @@
         SystemAddress,
         timestamp,
         @event
+    }
+
+    public static class EDSystemCache
+    {
+        public static async Task<bool> GetSystemCache(Dictionary<string, JsonElement> elementAsDictionary, IDatabase _rdb)
+        {
+            var reqProps = typeof(RequiredPropertiesForCache).GetEnumNames();
+
+            var requiredProperties = elementAsDictionary.Keys.Where(k => Enum.TryParse(typeof(RequiredPropertiesForCache), k, false, out _));
+            var missingProps = reqProps.Except(requiredProperties);
+
+            bool setCache = false;
+
+            if (!missingProps.Any())
+            {
+                await _rdb.StringSetAsyncWithRetries(
+                    $"SystemAddress:{elementAsDictionary["SystemAddress"].GetInt64()}",
+                    JsonSerializer.Serialize(new
+                    {
+                        SystemAddress = elementAsDictionary["SystemAddress"].GetInt64(),
+                        StarSystem = elementAsDictionary["StarSystem"].GetString(),
+                        StarPos = elementAsDictionary["StarPos"]
+                    }),
+                    TimeSpan.FromHours(10),
+                    flags: CommandFlags.FireAndForget
+                );
+
+                await _rdb.StringSetAsyncWithRetries(
+                    $"StarSystem:{elementAsDictionary["StarSystem"].GetString()}",
+                    JsonSerializer.Serialize(new
+                    {
+                        SystemAddress = elementAsDictionary["SystemAddress"].GetInt64(),
+                        StarSystem = elementAsDictionary["StarSystem"].GetString(),
+                        StarPos = elementAsDictionary["StarPos"]
+                    }),
+                    TimeSpan.FromHours(10),
+                    flags: CommandFlags.FireAndForget
+                );
+
+                setCache = true;
+            }
+
+            var importantProps = new[] { "StarPos", "StarSystem", "SystemAddress" };
+
+            if (!missingProps.Contains("SystemAddress"))
+            {
+                var cachedSystem = await _rdb.StringGetAsyncWithRetries($"SystemAddress:{elementAsDictionary["SystemAddress"].GetInt64()}");
+                if (cachedSystem != RedisValue.Null)
+                {
+                    var jel = JsonDocument.Parse(cachedSystem.ToString()).RootElement;
+                    elementAsDictionary["SystemAddress"] = jel.GetProperty("SystemAddress");
+                    elementAsDictionary["StarSystem"] = jel.GetProperty("StarSystem");
+                    elementAsDictionary["StarPos"] = jel.GetProperty("StarPos");
+
+                    setCache = true;
+                }
+            }
+            else if (!missingProps.Contains("StarSystem"))
+            {
+                var cachedSystem = await _rdb.StringGetAsyncWithRetries($"StarSystem:{elementAsDictionary["StarSystem"].GetString()}");
+                if (cachedSystem != RedisValue.Null)
+                {
+                    var jel = JsonDocument.Parse(cachedSystem.ToString()).RootElement;
+                    elementAsDictionary["SystemAddress"] = jel.GetProperty("SystemAddress");
+                    elementAsDictionary["StarSystem"] = jel.GetProperty("StarSystem");
+                    elementAsDictionary["StarPos"] = jel.GetProperty("StarPos");
+
+                    setCache = true;
+                }
+            }
+
+            return setCache;
+        }
+
+        public static async Task SetSystemCache(EDGameState gameState, IDatabase _rdb, bool setCache)
+        {
+            if (!setCache && gameState.SystemAddress.HasValue && gameState.SystemCoordinates.HasValue && !string.IsNullOrWhiteSpace(gameState.SystemName))
+            {
+                await _rdb.StringSetAsyncWithRetries(
+                    $"SystemAddress:{gameState.SystemAddress}",
+                    JsonSerializer.Serialize(new
+                    {
+                        SystemAddress = gameState.SystemAddress,
+                        StarSystem = gameState.SystemName,
+                        StarPos = gameState.SystemCoordinates
+                    }),
+                    TimeSpan.FromHours(10),
+                    flags: CommandFlags.FireAndForget
+                );
+
+                await _rdb.StringSetAsyncWithRetries(
+                    $"StarSystem:{gameState.SystemName}",
+                    JsonSerializer.Serialize(new
+                    {
+                        SystemAddress = gameState.SystemAddress,
+                        StarSystem = gameState.SystemName,
+                        StarPos = gameState.SystemCoordinates
+                    }),
+                    TimeSpan.FromHours(10),
+                    flags: CommandFlags.FireAndForget
+                );
+            }
+        }
     }
 }
--- Journal-Limpet/Jobs/EDSMUserUploader.cs
+++ Journal-Limpet/Jobs/EDSMUserUploader.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Minio;
 using Polly;
-using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -65,7 +64,7 @@ new SqlParameter("user_identifier", userIdentifier)
                     }
 
                     var userJournals = await db.ExecuteListAsync<UserJournal>(
-                        "SELECT * FROM user_journal WHERE user_identifier = @user_identifier AND ISNULL(JSON_VALUE(integration_data, '$.EDSM.lastSEntLineNumber'), '0') < last_processed_line_number AND ISNULL(JSON_VALUE(integration_data, '$.EDSM.fullySent'), 'false') = 'false' ORDER BY journal_date ASC",
+                        "SELECT * FROM user_journal WHERE user_identifier = @user_identifier AND ISNULL(JSON_VALUE(integration_data, '$.EDSM.lastSentLineNumber'), '0') < last_processed_line_number AND ISNULL(JSON_VALUE(integration_data, '$.EDSM.fullySent'), 'false') = 'false' ORDER BY journal_date ASC",
                         new SqlParameter("user_identifier", userIdentifier)
                     );
 
@@ -77,7 +76,7 @@ new SqlParameter("user_identifier", userIdentifier)
                     if (firstAvailableGameState != null)
                     {
                         var previousJournal = await db.ExecuteSingleRowAsync<UserJournal>(
-                            "SELECT TOP 1 * FROM user_journal WHERE user_identifier = @user_identifier AND journal_id < @journal_id AND last_processed_line_number > 0 AND integration_data IS NOT NULL ORDER BY journal_date DESC",
+                            "SELECT TOP 1 * FROM user_journal WHERE user_identifier = @user_identifier AND journal_id <= @journal_id AND last_processed_line_number > 0 AND integration_data IS NOT NULL ORDER BY journal_date DESC",
                             new SqlParameter("user_identifier", userIdentifier),
                             new SqlParameter("journal_id", firstAvailableGameState.JournalId)
                         );
@@ -487,73 +486,10 @@ new SqlParameter("user_identifier", userIdentifier)
 
         public static async Task<JsonElement> SetGamestateProperties(JsonElement element, EDGameState gameState, string commander)
         {
-            var _rdb = SharedSettings.RedisClient.GetDatabase(1);
             var elementAsDictionary = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(element.GetRawText());
 
-            var reqProps = typeof(RequiredPropertiesForCache).GetEnumNames();
-
-            var requiredProperties = elementAsDictionary.Keys.Where(k => System.Enum.TryParse(typeof(RequiredPropertiesForCache), k, false, out _));
-            var missingProps = reqProps.Except(requiredProperties);
-
-            bool setCache = false;
-
-            if (!missingProps.Any())
-            {
-                await _rdb.StringSetAsyncWithRetries(
-                    $"SystemAddress:{elementAsDictionary["SystemAddress"].GetInt64()}",
-                    JsonSerializer.Serialize(new
-                    {
-                        SystemAddress = elementAsDictionary["SystemAddress"].GetInt64(),
-                        StarSystem = elementAsDictionary["StarSystem"].GetString(),
-                        StarPos = elementAsDictionary["StarPos"]
-                    }),
-                    TimeSpan.FromHours(10),
-                    flags: CommandFlags.FireAndForget
-                );
-
-                await _rdb.StringSetAsyncWithRetries(
-                    $"StarSystem:{elementAsDictionary["StarSystem"].GetString()}",
-                    JsonSerializer.Serialize(new
-                    {
-                        SystemAddress = elementAsDictionary["SystemAddress"].GetInt64(),
-                        StarSystem = elementAsDictionary["StarSystem"].GetString(),
-                        StarPos = elementAsDictionary["StarPos"]
-                    }),
-                    TimeSpan.FromHours(10),
-                    flags: CommandFlags.FireAndForget
-                );
-
-                setCache = true;
-            }
-
-            var importantProps = new[] { "StarPos", "StarSystem", "SystemAddress" };
-
-            if (!missingProps.Contains("SystemAddress"))
-            {
-                var cachedSystem = await _rdb.StringGetAsyncWithRetries($"SystemAddress:{elementAsDictionary["SystemAddress"].GetInt64()}");
-                if (cachedSystem != RedisValue.Null)
-                {
-                    var jel = JsonDocument.Parse(cachedSystem.ToString()).RootElement;
-                    elementAsDictionary["SystemAddress"] = jel.GetProperty("SystemAddress");
-                    elementAsDictionary["StarSystem"] = jel.GetProperty("StarSystem");
-                    elementAsDictionary["StarPos"] = jel.GetProperty("StarPos");
-
-                    setCache = true;
-                }
-            }
-            else if (!missingProps.Contains("StarSystem"))
-            {
-                var cachedSystem = await _rdb.StringGetAsyncWithRetries($"StarSystem:{elementAsDictionary["StarSystem"].GetString()}");
-                if (cachedSystem != RedisValue.Null)
-                {
-                    var jel = JsonDocument.Parse(cachedSystem.ToString()).RootElement;
-                    elementAsDictionary["SystemAddress"] = jel.GetProperty("SystemAddress");
-                    elementAsDictionary["StarSystem"] = jel.GetProperty("StarSystem");
-                    elementAsDictionary["StarPos"] = jel.GetProperty("StarPos");
-
-                    setCache = true;
-                }
-            }
+            var _rdb = SharedSettings.RedisClient.GetDatabase(1);
+            bool setCache = await EDSystemCache.GetSystemCache(elementAsDictionary, _rdb);
 
             GameStateChanger.GameStateFixer(gameState, commander, elementAsDictionary);
 
@@ -576,32 +512,7 @@ new SqlParameter("user_identifier", userIdentifier)
             elementAsDictionary["_stationName"] = transientState.GetProperty("_stationName");
             elementAsDictionary["_shipId"] = transientState.GetProperty("_shipId");
 
-            if (!setCache && gameState.SystemAddress.HasValue && gameState.SystemCoordinates.HasValue && !string.IsNullOrWhiteSpace(gameState.SystemName))
-            {
-                await _rdb.StringSetAsyncWithRetries(
-                    $"SystemAddress:{gameState.SystemAddress}",
-                    JsonSerializer.Serialize(new
-                    {
-                        SystemAddress = gameState.SystemAddress,
-                        StarSystem = gameState.SystemName,
-                        StarPos = gameState.SystemCoordinates
-                    }),
-                    TimeSpan.FromHours(10),
-                    flags: CommandFlags.FireAndForget
-                );
-
-                await _rdb.StringSetAsyncWithRetries(
-                    $"StarSystem:{gameState.SystemName}",
-                    JsonSerializer.Serialize(new
-                    {
-                        SystemAddress = gameState.SystemAddress,
-                        StarSystem = gameState.SystemName,
-                        StarPos = gameState.SystemCoordinates
-                    }),
-                    TimeSpan.FromHours(10),
-                    flags: CommandFlags.FireAndForget
-                );
-            }
+            await EDSystemCache.SetSystemCache(gameState, _rdb, setCache);
 
             var json = JsonSerializer.Serialize(elementAsDictionary);
             return JsonDocument.Parse(json).RootElement;
