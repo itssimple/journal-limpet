e3a5c0e1eac142f99c8014eb0cce42032f87189e
NoLifeKing <webmaster@itssimple.se>
2021-03-01 11:24:45 +0100

    fix: Make a proper HTML version of the notification email. ^^;

--- Journal-Limpet/Jobs/SharedCode/SendLoginNotificationMethod.cs
+++ Journal-Limpet/Jobs/SharedCode/SendLoginNotificationMethod.cs
@@ -16,7 +16,7 @@ namespace Journal_Limpet.Jobs.SharedCode
 
 This is an automated email, since you have logged in to Journal Limpet at least once.
 
-I'm sorry that I have to send you this email, but we need you to log in to Journal-Limpet <https://journal-limpet.com> again.
+I'm sorry that I have to send you this email, but we need you to log in to Journal Limpet <https://journal-limpet.com> again.
 
 .. at least if you want us to be able to continue:
 - Storing your Elite: Dangerous journals
@@ -30,13 +30,35 @@ Regards,
 NoLifeKing85
 Journal Limpet";
 
+            var htmlEmail = @"<html>
+<head></head>
+<body>
+Hi there!<br />
+<br />
+This is an automated email, since you have logged in to <b>Journal Limpet</b> at least once.<br />
+<br />
+I'm sorry that I have to send you this email, but we need you to log in to <a href=""https://journal-limpet.com/Login"" target=""_blank"">Journal Limpet</a> again.<br />
+<br />
+.. at least if you want us to be able to continue:<br />
+- Storing your Elite: Dangerous journals<br />
+- Sync progress with other applications<br />
+<br />
+And if you don't want us to sync your account any longer, we'll delete your account after 6 months from your last fetched journal.<br />
+<br />
+This is the only email we will ever send you (every time that you need to login)<br />
+<br />
+Regards,<br />
+NoLifeKing85<br />
+<a href=""https://journal-limpet.com/"" target=""_blank"">Journal Limpet</a>
+</body></html>";
+
             var sendgridClient = new SendGridClient(configuration["SendGrid:ApiKey"]);
             var mail = MailHelper.CreateSingleEmail(
                 new EmailAddress("no-reply+account-notifications@journal-limpet.com", "Journal Limpet"),
                 new EmailAddress(user.NotificationEmail),
                 "Login needed for further journal storage",
                 email,
-               $"<html><head></head><body>{email.Replace("\n", "<br />\n")}</body></html>"
+                htmlEmail
             );
 
             await sendgridClient.SendEmailAsync(mail);
