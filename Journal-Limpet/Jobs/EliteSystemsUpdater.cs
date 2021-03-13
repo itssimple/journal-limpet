e1b9c4b5bd96b8b95093d038067a2f669a4fa124
NoLifeKing <webmaster@itssimple.se>
2021-03-14 00:57:48 +0100

    feat: New background job to update the local star system table.

---
+++ Journal-Limpet/Jobs/EliteSystemsUpdater.cs
@@ -0,0 +1,106 @@
+﻿using Hangfire;
+using Hangfire.Console;
+using Hangfire.Server;
+using Journal_Limpet.Shared;
+using Journal_Limpet.Shared.Database;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Journal_Limpet.Jobs
+{
+    public class EliteSystemsUpdater
+    {
+        [JobDisplayName("Elite System Updater")]
+        public static async Task UpdateEliteSystemsAsync(PerformContext context)
+        {
+            using (var rlock = new RedisJobLock($"EliteSystemsUpdater.UpdateEliteSystemsAsync"))
+            {
+                if (!rlock.TryTakeLock()) return;
+                context.WriteLine("Checking if we need to update the starsystem");
+
+                var _rdb = SharedSettings.RedisClient.GetDatabase(0);
+                var lastCachedUpdate = await _rdb.StringGetAsync("spansh:latestUpdate");
+
+                long systems = 0;
+
+                var url = "https://downloads.spansh.co.uk/galaxy_1day.json.gz";
+
+                if (lastCachedUpdate.IsNull)
+                {
+                    url = "https://downloads.spansh.co.uk/galaxy_7days.json.gz";
+                }
+
+                List<string> batchInserts = new List<string>();
+
+                using (var scope = Startup.ServiceProvider.CreateScope())
+                {
+                    var db = scope.ServiceProvider.GetRequiredService<MSSQLDB>();
+                    var configuration = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+                    var hc = SharedSettings.GetHttpClient(scope);
+
+                    var lastUpdateCheck = await hc.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+
+                    if (lastCachedUpdate.IsNull || lastCachedUpdate != lastUpdateCheck.Content.Headers.LastModified.ToString())
+                    {
+                        using (var beep = await hc.GetStreamAsync(url))
+                        using (var fileStream = new BufferedStream(beep))
+                        using (var gzipStream = new GZipStream(fileStream, CompressionMode.Decompress))
+                        using (var textReader = new StreamReader(gzipStream))
+                        {
+                            while (!textReader.EndOfStream)
+                            {
+                                var line = textReader.ReadLine();
+                                if (!string.IsNullOrWhiteSpace(line) && line.Trim() != "[" && line.Trim() != "]")
+                                {
+                                    line = line.Trim(',').Trim();
+                                    if (line.StartsWith("{") && line.EndsWith("}"))
+                                    {
+                                        systems++;
+                                        var sys = JsonSerializer.Deserialize<EDSystemData>(line);
+                                        var jsPos = JsonSerializer.Serialize(sys.Coordinates);
+
+                                        var sql = $@"({sys.Id64}, '{sys.Name.Replace("'", "''")}', '{jsPos}')";
+                                        batchInserts.Add(sql);
+
+                                        if (batchInserts.Count == 1000)
+                                        {
+                                            await ExecuteBatchInsert(batchInserts, db);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        if (batchInserts.Count > 0)
+                        {
+                            await ExecuteBatchInsert(batchInserts, db);
+                        }
+                    }
+
+                    await _rdb.StringSetAsyncWithRetries("spansh:latestUpdate", lastUpdateCheck.Content.Headers.LastModified.ToString(), TimeSpan.FromHours(23));
+                }
+
+                context.WriteLine("All done!");
+            }
+        }
+
+        private static async Task ExecuteBatchInsert(List<string> batchInserts, MSSQLDB db)
+        {
+            var batch = $@"BEGIN TRANSACTION;
+INSERT INTO EliteSystem (SystemAddress, StarSystem, StarPos)
+VALUES
+{string.Join(",\n", batchInserts)}
+COMMIT TRANSACTION";
+
+            await db.ExecuteNonQueryAsync(batch);
+            batchInserts.Clear();
+        }
+    }
+}
