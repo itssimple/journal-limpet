65fc63e6ddf3dbc8817c8c9971d7e4581934611d
NoLifeKing <webmaster@itssimple.se>
2021-05-14 14:34:46 +0200

    chore: Added empty test-project, for where I can add tests, whenever I fix the code to be testable.

---
+++ Journal-Limpet.Tests/TestBootstrapper.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace Journal_Limpet.Tests
+{
+    public class TestBootstrapper
+    {
+        [Fact]
+        public void Test1()
+        {
+            //ServiceCollection sc = new ServiceCollection();
+            //sc.AddJournalLimpetDependencies(null);
+        }
+    }
+}
