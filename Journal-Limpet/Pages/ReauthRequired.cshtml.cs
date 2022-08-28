fa35a97e4c8054f1f9315ee5f976450c5f799d42
NoLifeKing <webmaster@itssimple.se>
2022-08-28 15:28:20 +0200

    refactor: Added page to show some info about having to re-login.

---
+++ Journal-Limpet/Pages/ReauthRequired.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Journal_Limpet.Pages
+{
+    public class ReauthRequiredModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
